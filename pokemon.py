0759752345d4bf4bc55ceba964137982a2980e69
tom_nowak <tomasz.nowak.stw@gmail.com>
2018-12-24 13:27:10 +0100

    added Pokemon's method get_useful_numeric_parameters, which code duplication (deleted fill_numpy_array_with_normalized_parameters, shortened max_values_of_useful_numeric_parameters

--- pokemon.py
+++ pokemon.py
@@ -61,29 +61,31 @@ class Pokemon:
     def type_as_one_string(self):
         return ' '.join(sorted(self.types))
 
-    def fill_numpy_array_with_normalized_parameters(self, array: np.array, max_values_for_normalization: np.array):
-        array[0] = self.health / max_values_for_normalization[0]
-        array[1] = self.attack / max_values_for_normalization[1]
-        array[2] = self.defense / max_values_for_normalization[2]
-        array[3] = self.vulnerability_against["bug"] / max_values_for_normalization[3]
-        array[4] = self.vulnerability_against["dark"] / max_values_for_normalization[4]
-        array[5] = self.vulnerability_against["dragon"] / max_values_for_normalization[5]
-        array[6] = self.vulnerability_against["electric"] / max_values_for_normalization[6]
-        array[7] = self.vulnerability_against["fairy"] / max_values_for_normalization[7]
-        array[8] = self.vulnerability_against["fight"] / max_values_for_normalization[8]
-        array[9] = self.vulnerability_against["fire"] / max_values_for_normalization[9]
-        array[10] = self.vulnerability_against["flying"] / max_values_for_normalization[10]
-        array[11] = self.vulnerability_against["ghost"] / max_values_for_normalization[11]
-        array[12] = self.vulnerability_against["grass"] / max_values_for_normalization[12]
-        array[13] = self.vulnerability_against["ground"] / max_values_for_normalization[13]
-        array[14] = self.vulnerability_against["ice"] / max_values_for_normalization[14]
-        array[15] = self.vulnerability_against["normal"] / max_values_for_normalization[15]
-        array[16] = self.vulnerability_against["poison"] / max_values_for_normalization[16]
-        array[17] = self.vulnerability_against["psychic"] / max_values_for_normalization[17]
-        array[18] = self.vulnerability_against["rock"] / max_values_for_normalization[18]
-        array[19] = self.vulnerability_against["steel"] / max_values_for_normalization[19]
-        array[20] = self.vulnerability_against["water"] / max_values_for_normalization[20]
-        array[21] = self.capture_rate / max_values_for_normalization[21]
+    def get_useful_numeric_parameters(self) -> np.array:
+        return np.array([
+            self.health,
+            self.attack,
+            self.defense,
+            self.vulnerability_against["bug"],
+            self.vulnerability_against["dark"],
+            self.vulnerability_against["dragon"],
+            self.vulnerability_against["electric"],
+            self.vulnerability_against["fairy"],
+            self.vulnerability_against["fight"],
+            self.vulnerability_against["fire"],
+            self.vulnerability_against["flying"],
+            self.vulnerability_against["ghost"],
+            self.vulnerability_against["grass"],
+            self.vulnerability_against["ground"],
+            self.vulnerability_against["ice"],
+            self.vulnerability_against["normal"],
+            self.vulnerability_against["poison"],
+            self.vulnerability_against["psychic"],
+            self.vulnerability_against["rock"],
+            self.vulnerability_against["steel"],
+            self.vulnerability_against["water"],
+            self.capture_rate
+        ])
 
 
 class PokemonList(list):
@@ -105,35 +107,14 @@ class PokemonList(list):
         data = np.empty((len(self), max_numeric_values.size + 1))
         for index, pokemon in enumerate(self):
             data[index, 0] = types_to_float_map[pokemon.type_as_one_string()]
-            pokemon.fill_numpy_array_with_normalized_parameters(data[index, 1:], max_numeric_values)
+            data[index, 1:] = np.true_divide(pokemon.get_useful_numeric_parameters(), max_numeric_values)
         return data
 
     def max_values_of_useful_numeric_parameters(self) -> np.array:
-        array = np.empty(22)
-        array[0] = np.max([pokemon.health for pokemon in self])
-        array[1] = np.max([pokemon.attack for pokemon in self])
-        array[2] = np.max([pokemon.defense for pokemon in self])
-        array[3] = np.max([pokemon.vulnerability_against["bug"] for pokemon in self])
-        array[4] = np.max([pokemon.vulnerability_against["dark"] for pokemon in self])
-        array[5] = np.max([pokemon.vulnerability_against["dragon"] for pokemon in self])
-        array[6] = np.max([pokemon.vulnerability_against["electric"] for pokemon in self])
-        array[7] = np.max([pokemon.vulnerability_against["fairy"] for pokemon in self])
-        array[8] = np.max([pokemon.vulnerability_against["fight"] for pokemon in self])
-        array[9] = np.max([pokemon.vulnerability_against["fire"] for pokemon in self])
-        array[10] = np.max([pokemon.vulnerability_against["flying"] for pokemon in self])
-        array[11] = np.max([pokemon.vulnerability_against["ghost"] for pokemon in self])
-        array[12] = np.max([pokemon.vulnerability_against["grass"] for pokemon in self])
-        array[13] = np.max([pokemon.vulnerability_against["ground"] for pokemon in self])
-        array[14] = np.max([pokemon.vulnerability_against["ice"] for pokemon in self])
-        array[15] = np.max([pokemon.vulnerability_against["normal"] for pokemon in self])
-        array[16] = np.max([pokemon.vulnerability_against["poison"] for pokemon in self])
-        array[17] = np.max([pokemon.vulnerability_against["psychic"] for pokemon in self])
-        array[18] = np.max([pokemon.vulnerability_against["rock"] for pokemon in self])
-        array[19] = np.max([pokemon.vulnerability_against["steel"] for pokemon in self])
-        array[20] = np.max([pokemon.vulnerability_against["water"] for pokemon in self])
-        array[21] = np.max([pokemon.capture_rate for pokemon in self])
+        array = self[0].get_useful_numeric_parameters()
+        for pokemon in self[1:]:
+            array = np.maximum(array, pokemon.get_useful_numeric_parameters())
         return array
-
 
 def map_strings_to_numbers(strings: Set[str]) -> Dict[str, float]:
     step = 1.0 / len(strings)
