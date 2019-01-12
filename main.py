a163b8536f658fcf78eca2a6bd7ca42d953af64f
SaxMan96 <matidorobek@wp.pl>
2019-01-12 20:45:59 +0100

    draw_2D_plot added

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from pokemon import PokemonList
 import solver
-from plotter import draw_3D_plot
+from plotter import draw_3d_plot, draw_2d_plot
+
 
 def temporary_main():
     pokemons = PokemonList.from_file("data.csv")
@@ -8,9 +9,12 @@ def temporary_main():
     print(f"greedy_team: {greedy_team.names()}, goal function: {round(greedy_team.goal_function()[0][-1],2)}")
     # pokemon_data_array is unused now, can be used later to determine neighbors in terms of stats
     # print("pokemons as numpy array:", pokemons.normalized_data, sep='\n')
-    search_result = solver.simulated_annealing(pokemons, iterations=80_000, save_history=False)
-    # draw_3D_plot(search_result.results_history)
+    # search_result = solver.simulated_annealing(pokemons, iterations=8_000, save_history=True)
+    search_result = solver.random_search(pokemons, iterations=10_000_000, save_history=True, print_results=False)
     print(f"best score: {round(search_result.best_score,2)}, best team: {search_result.best_team.names()}")
+    draw_3d_plot(search_result.results_history)
+    draw_2d_plot(search_result.results_history)
+
 
 if __name__ == '__main__':
     temporary_main()
