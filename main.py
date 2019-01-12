190971ef460dc657b873290aee0d82dd034e758d
Mateusz Dorobek <17788237+SaxMan96@users.noreply.github.com>
2019-01-12 20:53:27 +0100

    changed num of iterations

--- main.py
+++ main.py
@@ -9,13 +9,8 @@ def temporary_main():
     print(f"greedy_team: {greedy_team.names()}, goal function: {round(greedy_team.goal_function()[0][-1],2)}")
     # pokemon_data_array is unused now, can be used later to determine neighbors in terms of stats
     # print("pokemons as numpy array:", pokemons.normalized_data, sep='\n')
-<<<<<<< HEAD
-    # search_result = solver.simulated_annealing(pokemons, iterations=8_000, save_history=True)
-    search_result = solver.random_search(pokemons, iterations=10_000_000, save_history=True, print_results=False)
-=======
     search_result = solver.simulated_annealing(pokemons, iterations=80_000, save_history=True)
     draw_3D_plot(search_result.results_history)
->>>>>>> ac1c68319bfcd2bd7e9a9077514aa0d52882c616
     print(f"best score: {round(search_result.best_score,2)}, best team: {search_result.best_team.names()}")
     draw_3d_plot(search_result.results_history)
     draw_2d_plot(search_result.results_history)
